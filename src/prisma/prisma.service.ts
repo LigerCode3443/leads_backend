1e2c1037a4cf07bee2f2508e783857cc6de103ef
LigerCode3443 <161658640+LigerCode3443@users.noreply.github.com>
2026-04-17 11:46:45 +0300

    fix: actually removed sqlite import

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
